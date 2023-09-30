0464506468c08202d919e49a40f654629e6744be
Lemein <lemeinannette@gmail.com>
2023-09-30 14:54:24 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningSeason = record.find(season => season.result === "W" && season.team === "Denver Broncos");
+    if (winningSeason) {
+      return winningSeason.year;
+    } else {
+      return undefined; 
+    }
+  }
+  const NflSeasons = [
+     {year: 2015, team: "Denver Broncos", result: "W" },
+    { year: 2014, team: "Green Bay Packers", result: "N/A" },
+    { year: 2013, team: "Denver Broncos", result: "W" },
+  ];
+  const WinningYear = superbowlWin(nflSeasons);
+  console.log(winningYear); 
